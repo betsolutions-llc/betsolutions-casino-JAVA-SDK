716fee83fa8cbe30d4088dd4adfec6e6673a5a09
tamaz bagdavadze <t.bagdavadze@betsolutions.com>
2019-08-02 15:28:58 +0400

    add backgammon tournament types

--- src/main/java/Betsolutions/Casino/SDK/TableGames/Backgammon/Enums/BackgammonTournamentType.java
+++ src/main/java/Betsolutions/Casino/SDK/TableGames/Backgammon/Enums/BackgammonTournamentType.java
@@ -1,4 +1,6 @@
 package Betsolutions.Casino.SDK.TableGames.Backgammon.Enums;
 
 public class BackgammonTournamentType {
+    public static final int Scheduled = 1;
+    public static final int SitAndGo = 2;
 }
